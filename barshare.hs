fee0679cecdc60f76a7d9585259c94b10665d5c4
Emilio Wuerges <wuerges@gmail.com>
2016-01-18 18:18:14 -0200

    phisics more or less ok

--- barshare.hs
+++ barshare.hs
@@ -2,52 +2,88 @@ import Haste
 import Haste.Graphics.Canvas
 import Haste.DOM
 import Data.List
-import Control.Monad
-
 
 data State = Paid | Unpaid
 data Item = Item { price :: Double, ammount :: Double, name :: String, code :: Int, state :: State }
-data Bubble = Bubble { pos :: Point, direction :: Vector, item :: Item}
+data Bubble =
+  Bubble { pos :: Point, direction :: Vector, item :: Item}
+
 
 testItem1 :: Item
-testItem1 = Item { price = 5.0, ammount = 7, name = "banana", code = 1, state = Unpaid }
+testItem1 = Item { price = 9.0, ammount = 7, name = "banana", code = 1, state = Unpaid }
 
 type Model = [Bubble]
 
 sortBubbles :: [Bubble] -> [Bubble]
-sortBubbles = sortOn (fst . pos)
+sortBubbles = sortOn (\b -> distance (0, 0) (pos b)) -- (fst . pos)
+
+radius :: Bubble -> Double
+radius b = price (item b) * ammount (item b)
 
 slow :: Double -> Double
-slow = (*0.9)
+slow = (* 0.95)
 
 move :: Point -> Bubble -> Bubble
 move lm b = b { pos = (x', y'), direction = (dx', dy') }
   where (dx, dy) = direction b
         (x, y) = pos b
-        (x', dx') = move1d (fst lm) (slow dx) x
-        (y', dy') = move1d (snd lm) (slow dy) y
+        (x', dx') = move1d (radius b) (fst lm - radius b) (slow dx) x
+        (y', dy') = move1d (radius b) (snd lm - radius b) (slow dy) y
 
-move1d :: Double -> Double -> Double -> (Double, Double)
-move1d l v s | s + v < 0     = (-s', -v)
-             | s + v > l     = (l + l - s', -v)
-             | otherwise     = (s', v)
+move1d :: Double -> Double -> Double -> Double -> (Double, Double)
+move1d le ld v s | s + v < le     = (le + le - s', -v)
+                 | s + v > ld     = (ld + ld - s', -v)
+                 | otherwise     = (s', v)
     where s' = s + v
 
-resolve :: Point -> [Bubble] -> Bubble -> IO [Bubble]
-resolve lm [] b = return [move lm b]
-resolve lm bs b = return $ (move lm b):bs
+
+negateV :: Vector -> Vector
+negateV (x, y) = (-x, -y)
+
+sumV :: Vector -> Vector -> Vector
+(x1, y1) `sumV` (x2, y2) = (x1 + x2, y1 + y2)
+
+minusV :: Vector -> Vector -> Vector
+v1 `minusV` v2 = v1 `sumV` (negateV v2)
+
+mulV :: Vector -> Double -> Vector
+(x1, y1) `mulV` s = (x1 * s, y1 * s)
+
+distance :: Point -> Point -> Double
+distance (ax, ay) (bx, by) = sqrt $ ((ax - bx) ** 2) + ((ay - by) ** 2)
+
+
+
+incItem b = b { item = item' }
+  where
+    i = item b
+    item' = i { code = code i + 1 }
+
+collide :: Bubble -> Bubble -> (Bubble, Bubble)
+collide a b | radius a + radius b < distance (pos a) (pos b) = (a, b)
+            | otherwise = ( incItem $ a { direction = dira' }
+                          , incItem $ b { direction = dirb' })
+  where
+    --(ax, ay) = pos a
+    --(bx, by) = pos b
+    dira' = direction a `minusV` (direction a `minusV` direction b `mulV` 1.05)
+    dirb' = direction b `minusV` (direction b `minusV` direction a `mulV` 1.05)
+
+resolve :: Point -> [Bubble] -> [Bubble]
+resolve _ []        = []
+resolve lm (b:[])   = [move lm b]
+resolve lm (a:b:bs) = (move lm a'):(resolve lm (b':bs))
+  where (a', b') = (collide a b)
 
 -- | Perform the physics of the bubbles
 physics :: Point -> Model -> IO Model
-physics d bs = do
-  bs' <- foldM (resolve d) [] (sortBubbles bs)
-  return bs'
+physics d bs = return $ resolve d (sortBubbles bs)
 
 -- | Basic picture of an Item
 itemShape :: Bubble -> Picture ()
 itemShape bubble = do
   checkColor (state i) $ fill $ circle p (price i * ammount i)
-  color (RGB 0 0 0) $ text p (name i)
+  color (RGB 0 0 0) $ text p (show $ code i)
   where checkColor Unpaid = color (RGB 255 0 0)
         checkColor Paid   = color (RGB 0 255 0)
         i = item bubble
@@ -64,7 +100,7 @@ scatter (w, h) n = zip xs ys
 
 -- | Places the items in the initial positions and attribute the state Unpaid
 initialPositions :: Point -> [Item] -> Model
-initialPositions wh items = [Bubble p (10, 10) i | (p, i) <- zip points items]
+initialPositions wh items = [Bubble p (100, 100) i | (p, i) <- zip points items]
   where points = scatter wh (length items)
 
 getDimensions :: Canvas -> IO Point
@@ -80,7 +116,7 @@ main = do
   Just e <- elemById "canvas"
   Just can <- fromElem e --ById "canvas"
   (w, h) <- getDimensions can
-  animate can (initialPositions (w, h) (replicate 20 testItem1))
+  animate can (initialPositions (w, h) (replicate 10 testItem1))
 
 -- | ...and use the render function to draw your image.
 --   The picture type is a monad, so you can compose several pictures easily
@@ -101,5 +137,5 @@ animate can bubbles = do
 
   d <- getDimensions can
   bubbles' <- physics d bubbles
-  setTimer (Once 20) $ animate can bubbles'
+  setTimer (Once 16) $ animate can bubbles'
   return ()
